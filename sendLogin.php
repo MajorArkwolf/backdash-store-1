8f48f604c00cc4c768b8f6bc97d90efbbcfe7de0
Peter <petercrabbe92@gmail.com>
2019-11-05 19:03:54 +0800

    fix blank username

--- sendLogin.php
+++ sendLogin.php
@@ -10,14 +10,15 @@
 
   if ( ! empty( $_POST ) ) {
       if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
-          // Getting submitted user data from database
-          $con = new mysqli($host, $userName, $password, $dbName);
-          $stmt = $con->prepare("SELECT * FROM Accounts WHERE email = ?");
-          $stmt->bind_param('s', $_POST['username']);
-          $stmt->execute();
-          $result = $stmt->get_result();
-      	  $user = $result->fetch_object();
-
+        if($_POST['username'] != '') {
+            // Getting submitted user data from database
+            $con = new mysqli($host, $userName, $password, $dbName);
+            $stmt = $con->prepare("SELECT * FROM Accounts WHERE email = ?");
+            $stmt->bind_param('s', $_POST['username']);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        	  $user = $result->fetch_object();
+        }
       	// Verify user password and set $_SESSION
       	if ($_POST['password'] == $user->password) {
       		$_SESSION['user_id'] = $user->ID;
