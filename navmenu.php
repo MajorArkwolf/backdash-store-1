98fe79c57a990a212ca6ee30fe9edb8fe777279a
Peter <petercrabbe92@gmail.com>
2019-11-05 17:44:36 +0800

    fix

--- navmenu.php
+++ navmenu.php
@@ -12,34 +12,19 @@ echo '<nav class="navbar">
         <a href="products.php">Products</a>
         <a href="categories.php">Categories</a>
       </div>
-    </div>';
-
-    if ( isset( $_SESSION['user_id'] ) ) {
-      echo'<button class="dropbtn">Account
-              <i class="fa fa-caret-down"></i>
-            </button>
-            <div class="dropdown-content">
-              <a href="details.php">Details</a>
-              <a href="logout.php">Log Out</a>
-            </div>
-          </div>';
-    } else {
-        echo'<button class="dropbtn">Account
-                <i class="fa fa-caret-down"></i>
-              </button>
-              <div class="dropdown-content">
-                <a href="login.php">Log In</a>
-                <a href="register.php">Register</a>
-              </div>
-            </div>';
-    }
-
-    echo'<div class="dropdown">
-
+    </div>
+    <div class="dropdown">
+      <button class="dropbtn">Account
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="details.php">Details</a>
+      </div>
+    </div>
     <div class="dropdown">
       <a href="aboutus.php">About Us</a>
     </div>
 
     <a href="javascript:void(0);" class="icon" onclick="toggleNavbar()">☰</a>
-  </nav>';
+  </nav>'
  ?>
