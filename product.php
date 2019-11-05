eb2708533e50c4a53526956a4cdfd02c3f1dc771
Sandro Stikić <sandro@stikic.me>
2019-11-05 17:59:08 +0800

    Update product

--- product.php
+++ product.php
@@ -29,7 +29,7 @@
 
             $mysqli = new mysqli("localhost", "X32019269", "X32019269", "X32019269");
 
-            if($stmt = $mysqli->prepare("select * from Products where Products.id = ?")) {
+            if($stmt = $mysqli->prepare("select P.id, P.name, P.description, P.price from Products P where P.id = ?")) {
                $stmt->bind_param("i", intval($_GET['id']));
                $stmt->execute();
                $stmt->bind_result($id, $name, $description, $price);
