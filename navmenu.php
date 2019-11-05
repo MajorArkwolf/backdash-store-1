3d06690efa5beb26ee0e7e9fd770885034ae458e
Peter <petercrabbe92@gmail.com>
2019-11-05 19:54:00 +0800

    login

--- navmenu.php
+++ navmenu.php
@@ -1,6 +1,6 @@
 <?php
   session_start();
-  if (isset($_SESSION['user_id'])) {
+  if (isset($_SESSION['loggedin'])) {
     $a = '<a href="details.php">Details</a>';
     $a .= '<a href="sendLogout.php">Log Out</a>';
   } else {
