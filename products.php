13b86523f773e7e2e98fe0d3a985bf90f497838a
Sandro Stikić <sandro@stikic.me>
2019-11-05 16:31:37 +0800

    Update products

--- products.php
+++ products.php
@@ -33,15 +33,13 @@
             $res = $mysqli->query("select * from Products");
 
             while ($row = $res->fetch_assoc()) {
-              echo "<div class='product'>";
-                echo "<a href='product.php?id=" . $row['id'] . "'>";
-                  echo "<div class='product-title'>" . $row['name'] . "</div>";
-                    echo "<div class='product-image'>";
-                      echo "<img src='img/" . $row['id'] . ".png' height='100' width='100'>";
-                    echo "</div>";
-                  echo "<div class='product-price'>" . "$" . $row['price'] . "</div>";
-                echo "</a>";
-              echo "</div>";
+              echo "<a class='product' href='product.php?id=" . $row['id'] . "'>";
+                echo "<div class='product-title'>" . $row['name'] . "</div>";
+                  echo "<div class='product-image'>";
+                    echo "<img src='img/" . $row['id'] . ".png' height='100' width='100'>";
+                  echo "</div>";
+                echo "<div class='product-price'>" . "$" . $row['price'] . "</div>";
+              echo "</a>";
             }
           ?>
         </div>
