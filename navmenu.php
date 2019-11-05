37cc51bfbe3c23710961d32196a847d43810065f
Peter <petercrabbe92@gmail.com>
2019-11-05 18:06:48 +0800

    test menu

--- navmenu.php
+++ navmenu.php
@@ -1,4 +1,13 @@
 <?php
+  session_start();
+  if (isset($_SESSION['user_id'])) {
+    $a = '<a href="details.php">Details</a>';
+    $a .= '<a href="sendLogout.php">Log Out</a>';
+  } else {
+    $a = '<a href="login.php">Login</a>';
+    $a .= '<a href="register.php">Register</a>';
+  }
+#$a = '<a href="login.php">Login</a>';
 echo '<nav class="navbar">
     <div class="dropdown active">
       <a href="index.php" class="dropbtn">Home</a>
@@ -17,8 +26,7 @@ echo '<nav class="navbar">
       <button class="dropbtn">Account
         <i class="fa fa-caret-down"></i>
       </button>
-      <div class="dropdown-content">
-        <a href="details.php">Details</a>
+      <div class="dropdown-content">' . $a . '
       </div>
     </div>
     <div class="dropdown">
