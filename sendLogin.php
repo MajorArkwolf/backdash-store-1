cd4cb9510ccd06f43edb65e0d530a6eba016532c
Peter <petercrabbe92@gmail.com>
2019-11-05 19:30:15 +0800

    php

--- sendLogin.php
+++ sendLogin.php
@@ -21,7 +21,7 @@
   $myusername = mysql_real_escape_string($myusername);
   $mypassword = mysql_real_escape_string($mypassword);
 
-  $sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
+  $sql="SELECT * FROM $tbl_name WHERE email='$myusername' and password='$mypassword'";
   $result=mysql_query($sql);
 
   // Mysql_num_row is counting table row
@@ -30,7 +30,6 @@
   // If result matched $myusername and $mypassword, table row must be 1 row
 
   if($count==1){
-
     // Register $myusername, $mypassword and redirect to file "login_success.php"
     session_register("myusername");
     session_register("mypassword");
