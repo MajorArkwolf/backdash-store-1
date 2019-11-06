c7bb62a4b212a321d12492948d166572108141f6
Sandro Stikić <sandro@stikic.me>
2019-11-06 20:11:00 +0800

    Update styles

---
+++ cart.php
@@ -0,0 +1,36 @@
+<?php
+  session_start();
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Backdash</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <div class="content">
+      <header>
+        <div>
+          <h1>Backdash</h1>
+        </div>
+      </header>
+      <?php
+          ob_start();
+          $navmenuGroup = "home";
+          include('navmenu.php');
+          $myStr = ob_get_contents();
+          ob_end_clean();
+          echo $myStr;
+      ?>
+      <div class="text-area">
+        <h3>
+          Shopping Cart
+        </h3>
+      </div>
+    </div>
+    <script src="navbar.js"></script>
+  </body>
+</html>
