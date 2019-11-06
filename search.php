be1be4a801601792d02536813f3623cec3a2c395
Sandro Stikić <sandro@stikic.me>
2019-11-06 16:13:17 +0800

    Update search

--- search.php
+++ search.php
@@ -32,9 +32,10 @@
             ini_set("display_errors", 1);
 
             $mysqli = new mysqli("localhost", "X32019269", "X32019269", "X32019269");
+            $search = "%{$_POST['text']}%";
 
             if($stmt = $mysqli->prepare("select P.id, P.name, P.price from Products P where P.name like ?")) {
-               $stmt->bind_param("s", "%" . $_GET['text'] . "%");
+               $stmt->bind_param("s", $search);
                $stmt->execute();
                $stmt->bind_result($id, $name, $price);
             }
