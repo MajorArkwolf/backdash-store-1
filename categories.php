ebfa782fb36bc81e01cccd987b55f49076ff61b6
Sandro Stikić <sandro@stikic.me>
2019-11-06 14:49:01 +0800

    Update categories

--- categories.php
+++ categories.php
@@ -18,17 +18,34 @@
         </div>
       </header>
       <?php
-          ob_start();
-          $navmenuGroup = "shop";
-          include('navmenu.php');
-          $myStr = ob_get_contents();
-          ob_end_clean();
-          echo $myStr;
+        ob_start();
+        $navmenuGroup = "shop";
+        include('navmenu.php');
+        $myStr = ob_get_contents();
+        ob_end_clean();
+        echo $myStr;
       ?>
       <div class="text-area">
-        <p>
-          Welcome to the Backdash store.
-        </p>
+        <div class="product-grid">
+          <?php
+            $mysqli = new mysqli("localhost", "X32019269", "X32019269", "X32019269");
+
+            if($stmt = $mysqli->prepare("select P.id, P.name, P.price from Products P")) {
+               $stmt->execute();
+               $stmt->bind_result($id, $name, $price);
+            }
+
+            while ($stmt->fetch()) {
+              echo "<a class='product' href='product.php?id=" . $id . "'>";
+                  echo "<div class='product-image'>";
+                    echo "<img src='img/" . $id . ".png' height='100' width='100'>";
+                  echo "</div>";
+                echo "<div class='product-title'>" . $name . "</div>";
+                echo "<div class='product-price'>" . "$" . $price . "</div>";
+              echo "</a>";
+            }
+          ?>
+        </div>
       </div>
     </div>
     <script src="navbar.js"></script>
