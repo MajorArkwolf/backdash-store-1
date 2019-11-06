4e23a5d5a700e44b74850f38f86c58b3e1b85a83
Peter <petercrabbe92@gmail.com>
2019-11-06 15:55:51 +0800

    fix headers

--- details.php
+++ details.php
@@ -37,6 +37,7 @@
              $stmt->bind_result($id, $name, $email, $address, $admin, $phone);
              $stmt->fetch();
           }
+          echo '<h3>Update Account Details</h3>'
           echo '<form id="update" onsubmit="" action="sendUpdate.php" method="POST">';
           echo '<p>Email: <input type="textbox" name="email" id="email" value="' . $email . '"></input></p>';
           echo '<p>Name: <input type="textbox" name="name" id="name" value="' . $name . '"></input></p>';
@@ -45,6 +46,7 @@
           echo '<p><button type="submit" id="submitbutton2">Submit</button>';
           echo '</form>';
           echo '<br></br>';
+          echo '<h3>Update Password</h3>'
           echo '<form id="updatePassword" onsubmit="" action="sendUpdatePassword.php" method="POST">';
           echo '<p>Password: <input type="password" name="userPassword" id="userPassword" onblur="CheckPassword()"></input></p>';
           echo '<p>Confirm Password: <input type="password" id="userPassword2" onblur="CheckPassword()"></input></p>';
