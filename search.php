39f746b8f09cd35797473ed37b5349b527f9086f
Sandro Stikić <sandro@stikic.me>
2019-11-06 16:09:58 +0800

    Update search

--- search.php
+++ search.php
@@ -33,7 +33,6 @@
 
             $mysqli = new mysqli("localhost", "X32019269", "X32019269", "X32019269");
 
-            echo $_GET['text'];
             if($stmt = $mysqli->prepare("select P.id, P.name, P.price from Products P where P.name like '%?%'")) {
                $stmt->bind_param("s", $_GET['text']);
                $stmt->execute();
