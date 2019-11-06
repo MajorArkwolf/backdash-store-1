28dc5cd3047b3f4039f4ecb88471ecf9ece7be03
Sandro Stikić <sandro@stikic.me>
2019-11-06 16:55:12 +0800

    Update search

--- navmenu.php
+++ navmenu.php
@@ -45,7 +45,7 @@ echo '<button class="dropbtn">Account
   echo '">';
   echo   '<a href="aboutus.php" class="dropbtn">About Us</a>
         </div>
-        <div class="search-container">
+        <div class="dropdown search-container">
     <form action="search.php">
       <input type="text" placeholder="Search..." name="text" required>
       <button type="submit"><i class="fa fa-search"></i></button>
