0e197372e6f155dea750610f5baedd491d2cd521
Sandro Stikić <sandro@stikic.me>
2019-11-06 23:12:00 +0800

    Update cart

--- addToCart.js
+++ addToCart.js
@@ -5,7 +5,7 @@ Storage.prototype.getObj = function(key) {
     return JSON.parse(this.getItem(key))
 }
 
-localStorage.setObj("cart", []1;
+localStorage.setObj("cart", []);
 
 function addToCart() {
     let quantity = parseInt(document.getElementById("quantity-picker"));
