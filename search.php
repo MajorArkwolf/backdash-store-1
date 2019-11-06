edc5e75e219405c7046004ef48a32b0cee1ae9fc
Sandro Stikić <sandro@stikic.me>
2019-11-06 16:11:57 +0800

    Update search

--- search.php
+++ search.php
@@ -33,8 +33,8 @@
 
             $mysqli = new mysqli("localhost", "X32019269", "X32019269", "X32019269");
 
-            if($stmt = $mysqli->prepare("select P.id, P.name, P.price from Products P where P.name like '%?%'")) {
-               $stmt->bind_param("s", $_GET['text']);
+            if($stmt = $mysqli->prepare("select P.id, P.name, P.price from Products P where P.name like ?")) {
+               $stmt->bind_param("s", "%" . $_GET['text'] . "%");
                $stmt->execute();
                $stmt->bind_result($id, $name, $price);
             }
