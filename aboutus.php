d1f56bcbb347b5eca59b7031eeda94625c9eb4ca
Peter <petercrabbe92@gmail.com>
2019-11-06 10:22:27 +0800

    fill out about me

--- aboutus.php
+++ aboutus.php
@@ -25,9 +25,17 @@
           echo $myStr;
       ?>
       <div class="text-area">
-        <p>
-          Welcome to the Backdash store.
-        </p>
+        <h2>Who are we?</h2>
+        <p>We are a company based in Australia who believes in the passion of fighting games. We strive to bring the best qaulity parts and equipment from across the globe into Australia and sell them at a reasonable price to help grow and nuture the fighting community.</p>
+        <br></br>
+        <h2>Our Approach to you!</h2>
+        <ul>
+          <li>We buy in bulk to save.</li>
+          <li>We sell those savings onto you.</li>
+          <li>Active sales and shipping team who gaurntee next day shipping if the product is in stock.</li>
+          <li>All products are bought from high quality suppliers.</li>
+          <li>If its just "not right", ship it back and we will replace it.</li>
+        </ul>
       </div>
     </div>
     <script src="navbar.js"></script>
