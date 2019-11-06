f10331af90cb9d7147f8bfd79b8ab9fdf3eeaa44
Sandro Stikić <sandro@stikic.me>
2019-11-06 22:49:27 +0800

    Update cart

--- addToCart.js
+++ addToCart.js
@@ -1,6 +1,17 @@
 function addToCart() {
     let quantity = document.getElementById("quantity-picker");
     let id = document.getElementById("id");
+    let cartStorage = localStorage.getItem("cart");
 
-    alert(quantity.value + ", " + id.value);
+    if (cart === null) {
+        let cart = [];
+        cart.push({id: id, quantity: quantity})
+        localStorage.setItem("cart", JSON.stringify(cart));
+        alert(cart);
+    } else {
+        let cart = JSON.parse(localStorage.getItem("cart"));
+        cart.push({id: id, quantity: quantity})
+        localStorage.setItem("cart", JSON.stringify(cart));
+        alert(cart);
+    }
 }
