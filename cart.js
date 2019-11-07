f83a5dc74734421e62fa9150af39199614db2fe4
Sandro Stikić <sandro@stikic.me>
2019-11-07 17:34:49 +0800

    Update cart

--- cart.js
+++ cart.js
@@ -6,10 +6,10 @@ Storage.prototype.getObj = function(key) {
 }
 
 Array.prototype.remove = function(from, to) {
-    var rest = this.slice((to || from) + 1 || this.length);
-    this.length = from < 0 ? this.length + from : from;
-    return this.push.apply(this, rest);
-};
+    var rest = this.slice((to || from) + 1 || this.length)
+    this.length = from < 0 ? this.length + from : from
+    return this.push.apply(this, rest)
+}
 
 function sortTable(table, col, reverse) {
     let tb = table.tBodies[0],
@@ -24,31 +24,32 @@ function sortTable(table, col, reverse) {
     });
 
     for(i = 0; i < tr.length; ++i) {
-        tb.appendChild(tr[i]);
+        tb.appendChild(tr[i])
     }
 }
 
 function removeItem(element) {
     let table = document.getElementById("cart")
-    let quantity = parseInt(element.parentNode.childNodes[0].value)
-    let id = parseInt(element.parentNode.parentNode.childNodes[4].innerHTML)
+    let row = element.parentNode.parentNode
+    let id = parseInt(row.childNodes[4].innerHTML)
 
-    let cart = localStorage.getObj("cart");
+    let cart = localStorage.getObj("cart")
 
     if (cart === null) {
-        localStorage.setObj("cart", []);
-        cart = localStorage.getObj("cart");
+        localStorage.setObj("cart", [])
+        cart = localStorage.getObj("cart")
     }
 
-    cart.remove(id);
-    localStorage.setObj("cart", cart);
+    cart.remove(id)
+    localStorage.setObj("cart", cart)
+    row.parentNode.removeChild(row)
 }
 
 function createElementFromHTML(htmlString) {
-    let div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
+    let div = document.createElement('div')
+    div.innerHTML = htmlString.trim()
 
-    return div.firstChild;
+    return div.firstChild
 }
 
 function updateQuantity(element) {
@@ -56,34 +57,34 @@ function updateQuantity(element) {
     let quantity = parseInt(element.parentNode.childNodes[0].value)
     let id = parseInt(element.parentNode.parentNode.childNodes[4].innerHTML)
 
-    let cart = localStorage.getObj("cart");
+    let cart = localStorage.getObj("cart")
 
     if (cart === null) {
-        localStorage.setObj("cart", []);
-        cart = localStorage.getObj("cart");
+        localStorage.setObj("cart", [])
+        cart = localStorage.getObj("cart")
     }
 
-    cart[id] = quantity;
+    cart[id] = quantity
     localStorage.setObj("cart", cart);
 
-    xmlhttp = new XMLHttpRequest();
+    xmlhttp = new XMLHttpRequest()
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             let data = JSON.parse(this.responseText)
 
             element.parentNode.parentNode.childNodes[3].innerHTML = "$" + data["totalPrice"]
         }
-    };
+    }
 
-    xmlhttp.open("GET","getData.php?id=" + id + "&quantity=" + quantity,true);
-    xmlhttp.send();
+    xmlhttp.open("GET","getData.php?id=" + id + "&quantity=" + quantity,true)
+    xmlhttp.send()
 }
 
 function updateTable(str) {
-    xmlhttp = new XMLHttpRequest();
+    xmlhttp = new XMLHttpRequest()
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            let cart = localStorage.getObj("cart");
+            let cart = localStorage.getObj("cart")
             let table = document.getElementById("cart").getElementsByTagName("tbody")[0]
             let data = JSON.parse(this.responseText)
 
@@ -106,18 +107,18 @@ function updateTable(str) {
             cell2.innerHTML = "$" + data["price"]
             cell3.innerHTML = "$" + data["totalPrice"]
             cell4.innerHTML = data["id"]
-            cell4.style.display = "none";
+            cell4.style.display = "none"
 
-            sortTable(document.getElementById("cart"), 4, false);
+            sortTable(document.getElementById("cart"), 4, false)
         }
-    };
+    }
 
-    xmlhttp.open("GET","getData.php?" + str,true);
-    xmlhttp.send();
+    xmlhttp.open("GET","getData.php?" + str,true)
+    xmlhttp.send()
 }
 
 function populateCart() {
-    let cart = localStorage.getObj("cart");
+    let cart = localStorage.getObj("cart")
 
     for (const [key, value] of Object.entries(cart)) {
         if (key != 0 && key != null && value > 0) {
