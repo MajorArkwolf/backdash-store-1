068321b8b573a2befedce40ac23eedbd958a853a
Sandro Stikić <sandro@stikic.me>
2019-11-07 23:29:14 +0800

    Update cart

--- buy.php
+++ buy.php
@@ -59,7 +59,7 @@
                         values(default, ?, ?, ?)";
               if($stmt = $mysqli->prepare($query)) {
                 $stmt->bind_param("ids", $_SESSION["id"], $sum,
-                  $_GET["card-name" . " " . $_GET["card-number"]]);
+                  $_GET["name" . " " . $_GET["card"]]);
                 $stmt->execute();
                 $transactionID = $mysqli->insert_id;
                 $stmt->close();
