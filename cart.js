23460e2118a80824aa05a4c78b40c6f6ec89c535
Sandro Stikić <sandro@stikic.me>
2019-11-07 16:45:41 +0800

    Update cart

--- cart.js
+++ cart.js
@@ -35,16 +35,17 @@ function updateTable(str) {
             let cell2 = row.insertCell(1)
             let cell3 = row.insertCell(2)
             let cell4 = row.insertCell(3)
+            let cell5 = row.insertCell(4)
 
             cell1.innerHTML = data["name"]
             cell3.innerHTML = "$" + data["price"]
             cell4.innerHTML = "$" + data["totalPrice"]
+            cell5.innerHTML = data["id"]
 
-            let hiddenId = createElementFromHTML('<input type="hidden" name="id" value="' + data["id"] + '">')
             let spinner = createElementFromHTML('<input class="quantity-picker-cart" type="number" name="quantity" value="' +
                                                 cart[data["id"]] + '" min="1" onclick="updateQuantity(this)">')
-            cell2.appendChild(hiddenId)
             cell2.appendChild(spinner)
+            cell5.style.display = "none";
         }
     };
 
